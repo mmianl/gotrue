347a2287a8ea8ab41e9621c9faaee9d5aeb177e2
Markus Müllner <m.muellner@firestart.com>
2023-06-15 14:25:24 +0200

    improve logging

---
+++ root_cmd.go
@@ -0,0 +1,58 @@
+package cmd
+
+import (
+	"context"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/supabase/gotrue/internal/conf"
+	"github.com/supabase/gotrue/internal/observability"
+)
+
+var configFile = ""
+
+var rootCmd = cobra.Command{
+	Use: "gotrue",
+	Run: func(cmd *cobra.Command, args []string) {
+		migrate(cmd, args)
+		serve(cmd.Context())
+	},
+}
+
+// RootCommand will setup and return the root command
+func RootCommand() *cobra.Command {
+	rootCmd.AddCommand(&serveCmd, &migrateCmd, &versionCmd, adminCmd())
+	rootCmd.PersistentFlags().StringVarP(&configFile, "config", "c", "", "the config file to use")
+	logrus.Debugf("set up root command with config %s", configFile)
+
+	return &rootCmd
+}
+
+func loadGlobalConfig(ctx context.Context) *conf.GlobalConfiguration {
+	if ctx == nil {
+		panic("context must not be nil")
+	}
+
+	config, err := conf.LoadGlobal(configFile)
+	if err != nil {
+		logrus.Fatalf("Failed to load configuration: %+v", err)
+	}
+
+	if err := observability.ConfigureLogging(&config.Logging); err != nil {
+		logrus.WithError(err).Error("unable to configure logging")
+	}
+
+	if err := observability.ConfigureTracing(ctx, &config.Tracing); err != nil {
+		logrus.WithError(err).Error("unable to configure tracing")
+	}
+
+	if err := observability.ConfigureMetrics(ctx, &config.Metrics); err != nil {
+		logrus.WithError(err).Error("unable to configure metrics")
+	}
+
+	return config
+}
+
+func execWithConfigAndArgs(cmd *cobra.Command, fn func(config *conf.GlobalConfiguration, args []string), args []string) {
+	fn(loadGlobalConfig(cmd.Context()), args)
+}
---
+++ serve_cmd.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"context"
+	"net"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/supabase/gotrue/internal/api"
+	"github.com/supabase/gotrue/internal/conf"
+	"github.com/supabase/gotrue/internal/storage"
+	"github.com/supabase/gotrue/internal/utilities"
+)
+
+var serveCmd = cobra.Command{
+	Use:  "serve",
+	Long: "Start API server",
+	Run: func(cmd *cobra.Command, args []string) {
+		serve(cmd.Context())
+	},
+}
+
+func serve(ctx context.Context) {
+
+	logrus.Debugf("running serve command")
+	config, err := conf.LoadGlobal(configFile)
+	if err != nil {
+		logrus.WithError(err).Fatal("unable to load config")
+	}
+
+	logrus.Debugf("opening database with config %s", config)
+	db, err := storage.Dial(config)
+	if err != nil {
+		logrus.Fatalf("error opening database: %+v", err)
+	}
+	defer db.Close()
+
+	api := api.NewAPIWithVersion(ctx, config, db, utilities.Version)
+
+	addr := net.JoinHostPort(config.API.Host, config.API.Port)
+	logrus.Infof("GoTrue API started on: %s", addr)
+
+	api.ListenAndServe(ctx, addr)
+}
